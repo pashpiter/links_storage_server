4a0f53409d4bcf47ffefac7d74e35db01f770ce1
Pavel Drovnin <pavel98rus@gmail.com>
2023-09-14 04:15:45 +0300

    fix return response

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import redis
 import tldextract
 import uvicorn
 from fastapi import Body, FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -12,6 +14,7 @@ REDIS_DATA_URL = "redis://localhost:6379"
 
 
 async def connection() -> redis.Redis:
+    """Настройка подключения к Redis"""
     conn = redis.StrictRedis(host='localhost', port=6379, db=0)
     return conn
 
@@ -19,7 +22,7 @@ async def connection() -> redis.Redis:
 @app.post('/visited_links')
 async def post_handler(
     links: Annotated[list[str], Body(embed=True)]
-) -> dict:
+) -> JSONResponse:
     """Получение сайтов из POST-запроса и внесение их в Redis"""
     try:
         conn = await connection()
@@ -30,13 +33,13 @@ async def post_handler(
             ext = tldextract.extract(link)
             domains.append(f'{ext.domain}.{ext.suffix}')
         conn.lpush(n, *set(domains))
-        return {"status": "ok"}
+        return JSONResponse({"status": "ok"}, status_code=201)
     except Exception as e:
-        return {"status": str(e)}
+        return JSONResponse({"status": str(e)}, status_code=400)
 
 
 @app.get('/visited_domains')
-async def get_handler(since: str, to: str) -> dict:
+async def get_handler(since: str, to: str) -> JSONResponse:
     """Получение списка посещенных сайтов за определенное время"""
     try:
         conn = await connection()
@@ -44,9 +47,9 @@ async def get_handler(since: str, to: str) -> dict:
         domains = []
         [(domains.extend(conn.lrange(id, 0, -1))) for id in ids]
         content = {"domains": sorted(set(domains)), "status": "ok"}
-        return content
+        return JSONResponse(jsonable_encoder(content), status_code=200)
     except Exception as e:
-        return {"status": str(e)}
+        return JSONResponse({"status": str(e)}, status_code=400)
 
 
 if __name__ == "__main__":
